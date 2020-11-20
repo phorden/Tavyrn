4632e6f16eabb6e1d6fa86043ef28477d2cd7bff
Dean Harmon <deanrharmon@gmail.com>
2020-11-20 12:16:19 -0600

    random chat simulator now fundamentally working.

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -11,29 +11,29 @@ const userSamples = [
 
 const textSamples = [
     {
-        id: 1,
+        id: 0,
         value: 'random reponse'
     },
     {
-        id: 2,
+        id: 1,
         value: 'I like trains!'
     },
     {
-        id: 3,
+        id: 2,
         value: 'They are so cute!'
     },
     {
-        id: 4,
+        id: 3,
         value: 'Can we watch something else? This isn\'t really my thing'
     },
     {
-        id: 5,
+        id: 4,
         value: 'I am getting sleepy.'
     }
 ];
 
 const Chat = () => {
-    const [chatList, setChatList]: any = useState([{user: 'bob', text: {id: 1, value: 'hello, world!'}}]);
+    const [chatList, setChatList]: any = useState([{user: 'bob', text: {id: 5, value: 'hello, world!'}}]);
 
     const simulateChat = () => {
         const chatPostDelay = Math.floor(Math.random() * Math.floor(5000));
@@ -41,37 +41,35 @@ const Chat = () => {
         const getTextIndex = Math.floor(Math.random() * Math.floor(textSamples.length - 1));
         const randomText = textSamples.find(text => text.id === getTextIndex);
 
-        console.log("random text: ", randomText);
-
         const getRandomUser: number = Math.floor(Math.random() * Math.floor(textSamples.length - 1));
         const randomUser = userSamples[getRandomUser];
 
-        console.log("random user: ", randomUser);
-
         const addMessageToList = () => {
-            const updatedChatList : object[] = [...chatList];
-            updatedChatList.push({user: randomUser, text: randomText});
+            const updatedChatList : object[] = [
+                ...chatList,
+                {user: randomUser, text: randomText}
+            ];
 
             setChatList(updatedChatList);
         }
 
-        setInterval(() => {
+        setTimeout(() => {
             addMessageToList();
         }, chatPostDelay);
     };
 
-    // useEffect(() => {
-    //     if (chatList.length) {
-    //         simulateChat();
-    //     }
-    // }, [chatList]);
+    useEffect(() => {
+        if (chatList.length) {
+            simulateChat();
+        }
+    }, [chatList, simulateChat]);
 
     const messages = (
         <div>
             {
-                chatList.map((message: any) => {
+                chatList.map((message: any, index: number) => {
                     return (
-                        <div className="message-wrapper" key={message.text.id}>
+                        <div className="message-wrapper" key={index}>
                             <h4 className="name">{message.user}</h4>
                             <p>{message.text.value}</p>
                         </div>
