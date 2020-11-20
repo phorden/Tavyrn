baa740339e55167d1b8b5d1481e3263627f66078
Dean Harmon <deanrharmon@gmail.com>
2020-11-19 18:33:07 -0600

    added basic routes & switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <Link to="/">Lobby</Link>
+            <Link to="/create-room">Create Viewing Room</Link>
+          </nav>
+        </header>
+      </div>
+
+      <Switch>
+        <Route path='/create-room'>
+          <CreateRoom />
+        </Route>
+        <Route path='/'>
+          <Lobby />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
